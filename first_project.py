b71aff53f57555a622c4c124cf9d1bc2d73079a3
gorakh-bansode <107914136+gorakh-bansode@users.noreply.github.com>
2025-11-10 19:13:29 +0530

    Update first_project.py
    first_project.py file updated

--- first_project.py
+++ first_project.py
@@ -1,1 +1,9 @@
+fruits = ["apple", "banana", "cherry"]
+print("Initial list:", fruits)
 
+fruits.append("orange")
+print("After adding orange:", fruits)
+
+print("Iterating through fruits:")
+for fruit in fruits:
+    print(fruit)
